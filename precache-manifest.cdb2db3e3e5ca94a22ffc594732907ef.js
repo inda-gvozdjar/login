28ea2cc1e0f65abfba276e8f9360839715c00be7
Inda Gvozdjar <inda.gvozdjar@gmail.com>
2020-07-30 16:12:48 +0200

    Updates

---
+++ precache-manifest.cdb2db3e3e5ca94a22ffc594732907ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dd4bf1fbd13f2547e5805ceffb8c2d",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "90c3e1c8aa8cfa05b204",
+    "url": "/login/static/css/main.deb7f57f.chunk.css"
+  },
+  {
+    "revision": "d8a65e57d074d537eea9",
+    "url": "/login/static/js/2.7684bd71.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/login/static/js/2.7684bd71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c3e1c8aa8cfa05b204",
+    "url": "/login/static/js/main.e82d76ca.chunk.js"
+  },
+  {
+    "revision": "20e65eafa9d56adae3b5",
+    "url": "/login/static/js/runtime-main.71de4d78.js"
+  },
+  {
+    "revision": "3816d835095c280bfc1dab998cfc51c2",
+    "url": "/login/static/media/login.3816d835.svg"
+  }
+]);
